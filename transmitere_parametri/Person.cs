9b60f65fa1f8ddb7e2df15a8c736703ea96c4548
Iosif Turcu <iosif.turcu@emmanuel.ro>
2022-01-14 09:01:21 +0200

    pasare parametrii cu referinta

---
+++ transmitere_parametri/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transmitere_parametri
+{
+    class Person
+    {
+		public Person(string firstName, string lastName)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+		}
+		public string FirstName
+		{
+			get;
+			set;
+		}
+
+		public string LastName
+		{
+			get;
+			set;
+		}
+
+	}
+}
---
+++ transmitere_parametri/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace transmitere_parametri
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 10;
+			Console.WriteLine($"Before Increment :: i={i}");
+			IncrementByRef(ref i);
+			Console.WriteLine($"After Increment :: i={i}");
+            Console.WriteLine("-------------------------------------");
+			Person p = new Person("John", "Doe");
+            Console.WriteLine($"Before ChangePersonName :: full name = {p.FirstName} {p.LastName}");
+			ChangePersonNameByRef(ref p);
+			Console.WriteLine($"Before ChangePersonName :: full name = {p.FirstName} {p.LastName}");
+		}
+
+		static void IncrementByRef(ref int i)
+		{
+			i = i + 1;
+			Console.WriteLine($"Inside Increment :: i={i}");
+		}
+
+		static void ChangePersonNameByRef(ref Person p)
+        {
+            Console.WriteLine($"Inside change ChangePersonName {p.FirstName} {p.LastName}");
+			//p.FirstName = "Changed John";
+			p = new Person("Changed John", "Doe");
+			Console.WriteLine($"Inside change ChangePersonName {p.FirstName} {p.LastName}");
+		}
+	}
+}
